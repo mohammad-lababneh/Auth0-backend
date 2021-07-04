bd3f6d45d2ab623920172c0d109d0ddac65fe278
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-07-04 23:26:27 +0300

    class11

--- server.js
+++ server.js
@@ -1,24 +1,60 @@
+// require('dotenv').config();
+// const express = require('express');
+// const cors = require('cors');
+// const jwt = require('jsonwebtoken');
+// const jwksClient = require('jwks-rsa');
+// const app = express();
+// app.use(cors());
+
+// const PORT = process.env.PORT || 3001;
+
+// app.get('/test', (request, response) => {
+
+//   // TODO: 
+//   // STEP 1: get the jwt from the headers
+//   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+//   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
+//   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+
+// })
+
+// app.listen(PORT, () => console.log(`listening on ${PORT}`));
 'use strict';
-
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
+const app=express();
+const cors=require('cors');
+const jwt=require('jsonwebtoken');
+const jwksClient=require('jwks-rsa');
+require('dotenv').config();
 
-const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const client = jwksClient({
+    // this url comes from your app on the auth0 dashboard 
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  });
 
-app.get('/test', (request, response) => {
+// this is a ready to use function
+const getKey=(header, callback)=>{
+    client.getSigningKey(header.kid, function(err, key) {
+        const signingKey = key.publicKey || key.rsaPublicKey;
+        callback(null, signingKey);
+      });
+}
 
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+// 'Bearer ;alsdkj;laskd;lkasd;lkl'
+app.get('/authorize',(req,res)=>{
+    const token=req.headers.authorization.split(' ')[1];
+    jwt.verify(token,getKey,{},(err,user)=>{
+        if(err){
+            res.send('invalid token');
+        }
+        res.send(user)
+    })
+    res.send(token);
+});
 
+app.listen(process.env.PORT,()=>{
+    console.log(`listening to port: ${process.env.PORT}`);
 })
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
