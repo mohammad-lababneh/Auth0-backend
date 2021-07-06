7f191fcda0d747ef5ee038ee2488c532314c3608
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-07-06 23:01:25 +0300

    class12

--- models/user.model.js
+++ models/user.model.js
@@ -19,31 +19,31 @@ const userModel = mongoose.model('userSchema',userSchema)
 
 
 
-// const seedUser=()=>{
+const seedUser=()=>{
 
 
-//     const feLawsOfPower = {
-//         name:'48 laws of power',
-//         description:"any thing",
-//         status:"any thing"
-//     }
-//     const art0fwar = {
-//         name:'48 laws of power',
-//         description:"any thing",
-//         status:"any thing"
-//     }
-//     const mySystem = {
-//         name:'48 laws of power',
-//         description:"any thing",
-//         status:"any thing"
-//     }
+    const feLawsOfPower = {
+        name:'48 laws of power',
+        description:"any thing",
+        status:"any thing"
+    }
+    const art0fwar = {
+        name:'48 laws of power',
+        description:"any thing",
+        status:"any thing"
+    }
+    const mySystem = {
+        name:'48 laws of power',
+        description:"any thing",
+        status:"any thing"
+    }
 
-//     const mohammad = new userModel({
-//         email:'lababnehmohammad96@gmail.com',
-//         books:[mySystem,art0fwar,feLawsOfPower]
-//     })
-//     mohammad.save();
-//     console.log(mohammad);
-//     return (mohammad)
-// }
-module.exports=userModel;
+    const mohammad = new userModel({
+        email:'lababnehmohammad96@gmail.com',
+        books:[mySystem,art0fwar,feLawsOfPower]
+    })
+    mohammad.save();
+    console.log(mohammad);
+    return (mohammad)
+}
+module.exports=seedUser;
